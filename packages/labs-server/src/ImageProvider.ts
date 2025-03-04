63b5e612d74268a84daa2b2622013930290a6f05
Bracketsrk <bracketsrk@gmail.com>
2025-03-04 00:20:08 -0800

    Changed ImageDocument.user to .auther

--- packages/labs-server/src/ImageProvider.ts
+++ packages/labs-server/src/ImageProvider.ts
@@ -4,16 +4,15 @@ interface User {
     _id: string;
     username: string;
     email: string;
-  }
+}
 
 interface ImageDocument {
     _id: string;
     src: string;
     name: string;
-    user: string | User | null;
+    author: string | User | null; 
     likes: number;
 }
-
 
 export class ImageProvider {
     constructor(private readonly mongoClient: MongoClient) {}
@@ -21,6 +20,7 @@ export class ImageProvider {
     async getAllImages(): Promise<ImageDocument[]> { 
         const imageCollectionName = process.env.IMAGES_COLLECTION_NAME;
         const userCollectionName = process.env.USERS_COLLECTION_NAME;
+        
         if (!imageCollectionName) {
             throw new Error("Missing IMAGES_COLLECTION_NAME from environment variables");
         }
@@ -31,30 +31,31 @@ export class ImageProvider {
         const imagesCollection = this.mongoClient.db().collection<ImageDocument>(imageCollectionName); 
         const usersCollection = this.mongoClient.db().collection<User>(userCollectionName);
 
-        
         const images = await imagesCollection.find().toArray();
 
-        // Denormalize the user field for each image
         const denormalizedImages = await Promise.all(
             images.map(async (image) => {
-                if (typeof image.user === "string") {
-                    // Fetch the full user document by the user ID if user is a string
-                    const user = await usersCollection.findOne({ _id: image.user });
-        
-                    if (user) {
-                    // Replace the user ID with the full user object
-                    image.user = user; 
-                    } else {
-                    // If no user is found, set user to null
-                    image.user = null;
+                if (typeof image.author === "string") {
+                    try {
+                        const user = await usersCollection.findOne({ _id: image.author });
+
+                        if (user) {
+                            image.author = user;
+                        } else {
+                            image.author = null;
+                        }
+                    } catch (error) {
+                        console.error(`Error fetching user with ID ${image.author}:`, error);
+                        image.author = null;
                     }
+                } else if (image.author === null) {
+                    image.author = null;
                 }
-    
+
                 return image;
-          })
+            })
         );
 
         return denormalizedImages;
-        // return collection.find().toArray(); // Without any options, will by default get all documents in the collection as an array.
     }
 }
